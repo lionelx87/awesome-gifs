8ff8da7331fee4b6fc03508d81d6ce04a440724b
Lionel Acosta <lionel.acosta@gm2dev.com>
2022-10-05 02:00:02 -0300

    add: prop-types

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 export const GifItem = ({ title, url, id }) => {
 
   const onClick = () => {
@@ -10,4 +11,9 @@ export const GifItem = ({ title, url, id }) => {
         <p>{ title }</p>
     </div>
   )
+}
+
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
 }
